c65297d3ba8f4bde3b330e7d3ccf4da1ee6aabc1
Peter Li <peterleepersonal@gmail.com>
2020-06-12 16:22:58 -0400

    chore: generate source maps

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,26 @@ const loaders = {
   rules: [{
     test: /\.(js|jsx)$/,
     exclude: /node_modules/,
-    use: {
-      loader: "babel-loader",
-    },
+    use: { loader: 'babel-loader' },
   }],
 };
 
 module.exports = [{
-    entry: './src/main/index.js',
-    output: {
-      filename: 'main.js',
-      path: path.join(__dirname, 'docs'),
-    },
-    module: loaders,
+  devtool: 'source-map',
+  entry: './src/main/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, 'docs'),
   },
-  {
-    entry: './src/worker/index.js',
-    output: {
-      filename: 'worker.js',
-      path: path.join(__dirname, 'docs'),
-    },
-    module: loaders,
+  module: loaders,
+},
+{
+  devtool: 'source-map',
+  entry: './src/worker/index.js',
+  output: {
+    filename: 'worker.js',
+    path: path.join(__dirname, 'docs'),
   },
+  module: loaders,
+},
 ];
